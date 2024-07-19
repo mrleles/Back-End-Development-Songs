1ca715db45cb290c284c66e55df058996d112019
Marlos Leles <marlosleles@gmail.com>
2024-07-19 10:45:34 -0400

    health and count added

--- backend/routes.py
+++ backend/routes.py
@@ -51,3 +51,11 @@ def parse_json(data):
 ######################################################################
 # INSERT CODE HERE
 ######################################################################
+@app.route("/health")
+def health():
+    return {"status":"OK"}, 200
+
+@app.route("/count")
+def count():
+    count = len(songs_list)
+    return {"count":count}, 200
